c6ee273bc013882a839f574b8abab8b7bc51bc61
Ravi Pal <rp789742@gmail.com>
2022-08-27 20:42:10 +0530

    function for pallindrom

--- Pallindrom/index.js
+++ Pallindrom/index.js
@@ -1,7 +1,7 @@
 const pallindrom = (str) => {
   let isPallindrom = false;
-  if(typeof str!="string"){
-    str=str.toString()
+  if (typeof str != "string") {
+    str = str.toString();
   }
   for (let i = 0, k = str.length - 1; i < str.length / 2; i++, k--) {
     if (str[i] == str[k]) {
