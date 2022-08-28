dc501785fa6400e7f37c94f756dc22c6c42b4a0e
Ravi Pal <rp789742@gmail.com>
2022-08-28 20:41:55 +0530

    created a function for checking prime no

---
+++ prime_no/index.js
@@ -0,0 +1,20 @@
+// Prime number
+
+const prime=(num)=>{
+
+    if(isNaN(num)||num==''){
+        return NaN
+    }
+    if(num==0){
+        return 0
+    }
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num%i==0){
+            return false
+        }
+    }
+    return true
+
+}
+
+console.log(prime());
