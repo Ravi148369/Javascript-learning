1e06ac74ac1d86c0b3d8faa7ee69efd6c141fd1a
Ravi Pal <rp789742@gmail.com>
2022-09-01 16:21:17 +0530

    todo list updated with array of object

--- todo_list/index.js
+++ todo_list/index.js
@@ -1,28 +1,37 @@
-const person={
-    task:[],
-    add:()=>{
-        const task={
-            taskname:document.getElementById('task').value,
-            status:'incomplete'
-        }
-        person.task.push(task);
-        person.render(person.task)
+let tasks = {
+  element: document.getElementById("items-list"),
+  items: [],
+  task: {
+    is_deleted: false,
+    priority: {
+      weigth: 3,
+      description: "high",
     },
-    render:(tasks)=>{
-        const div=document.getElementById('div')
-        div.innerHTML=''
-        tasks.map((element,index)=>{
-            div.innerHTML+=`<div class='taskdiv'>
-                                <p>${element.taskname}</p>
-                                <button onclick=person.remove(this,${index})>delete</button>
-                            </div
-                            `
-        })
-    },
-    remove:(element,index)=>{
-        element.parentElement.remove()
-        delete(person.task[index])
-        person.task=person.task.filter(value=>value)
-        person.render(person.task)
+  },
+  add: function (task) {
+    if (task.description !== "") {
+      this.task.description = task.description;
+      this.task.id = this.items.length + 1;
+      task = Object.assign(task, this.task);
+      this.items.push(task);
+      this.render(task);
+      return;
     }
-}
+    alert("please enter task");
+  },
+  render: function (task) {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    const p = document.createElement("p");
+    p.innerText = task.description;
+    button.innerText = "delete";
+    button.onclick = () => this.remove(task.id, p);
+    li.appendChild(p);
+    li.appendChild(button);
+    this.element.append(li);
+  },
+  remove: function (taskId, p) {
+    this.items[taskId - 1].is_deleted = true;
+    p.style.textDecoration = "line-through";
+  },
+};
