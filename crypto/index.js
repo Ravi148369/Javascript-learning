1b586a92f75db330c17151827d6f9d6b4d0b435c
Ravi Pal <rp789742@gmail.com>
2022-10-06 10:43:49 +0530

    added chart using chartjs

--- crypto/index.js
+++ crypto/index.js
@@ -4,6 +4,9 @@
         viewButton: document.querySelector('.view-more-btn'),
         main: document.querySelector('main'),
         input: document.querySelector('input'),
+        loader: document.querySelector('#loader'),
+        model: document.querySelector('.model'),
+        select: document.querySelector('select'),
         request: new XMLHttpRequest(),
         data: '',
         count: 20,
@@ -18,25 +21,27 @@
             return elements
         },
         getData: function(count = 20){
+            this.loader.classList.add('loader')
             this.request.open('get', 'https://api.coincap.io/v2/assets', true)
-            this.request.onload = function(){
-                if(this.status == 200){
+            this.request.onload = (e)=>{
+                if(e.target.status == 200){
                     try{
-                        crypto.data = (JSON.parse(this.response)).data
-                        const renderData = crypto.data.slice(0,count)
-                        crypto.tbody.innerHTML = ''
+                        this.data = (JSON.parse(e.target.response)).data
+                        const renderData = this.data.slice(0,count)
+                        this.tbody.innerHTML = ''
+                        this.loader.classList.remove('loader')
                         renderData.forEach(value => {
-                            crypto.render(value)
+                            this.render(value)
                         })
                     }catch(error){
                         console.log('error' + error);
                     }
                 }
                 else{
-                    crypto.notFound()
+                    this.notFound()
                 }
             }
-            this.request.onerror=function(){
+            this.request.onerror=function(){    
                 crypto.notFound()
             }
             this.request.send()
@@ -58,6 +63,9 @@
             volume.textContent = `${this.converter(value.volumeUsd24Hr) ?? "0"}`
             change.textContent = `${parseFloat(value.changePercent24Hr).toFixed(2) ?? "0"}`
             value.changePercent24Hr < 0?change.classList.add('red'):change.classList.add('green')
+            tr.addEventListener('click',()=>{
+                this.getChart(value.id)
+            })
             imgDiv.append(img,p)
             name.append(imgDiv)
             tr.append(rank, name, price, marketcap, vwap, supply, volume, change)
@@ -85,6 +93,19 @@
                     }
                 })
             }
+            if(this.model){
+                this.model.addEventListener('click',(e)=>{
+                    if(e.target == this.model){
+                        window.scroll = 'scroll'
+                        this.model.style.display = 'none'
+                    }
+                })
+            }
+            if(this.select){
+                this.select.addEventListener('change',()=>{
+                    this.getChart()
+                })
+            }
         },
         notFound: function(){
             if(this.main){
@@ -95,59 +116,66 @@
             }
         },
         converter: function(labelValue){
-            // Nine Zeroes for Billions
             const formatter = new Intl.NumberFormat('en-US', {
                 style: 'currency',
                 currency: 'USD',
             });
             return Math.abs(Number(labelValue)) >= 1.0e+9
-
                 ? formatter.format((Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2))  + "B"
-                // Six Zeroes for Millions 
                 : Math.abs(Number(labelValue)) >= 1.0e+6
-
                 ? formatter.format((Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2))  + "M"
-                // Three Zeroes for Thousands
                 : (Math.abs(Number(labelValue)) >= 1.0e+3) 
-
                 ? formatter.format((Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2)) + "K"
-
                 : Math.abs(Number(labelValue)).toFixed(2) ;
         },
-        // getChart(id){
-        //     let chartData = ''
-        //     fetch(`https://api.coincap.io/v2/assets/${id}/history?interval=d1`)
-        //     .then(value=>value.json()
-        //     .then(value=>chartData = value.data.slice(-6))
-        //     )
-        //     const labels = [
-        //         '29-09-2022',    
-        //         '30-09-2022',
-        //         '01-10-2022',
-        //         '02-10-2022',
-        //         '03-10-2022',
-        //         '04-10-2022'
-        //       ];
-            
-        //     const data = {
-        //         labels: labels,
-        //         datasets: [{
-        //             label: 'PRIZE',
-        //             backgroundColor: 'rgb(255, 99, 132)',
-        //             borderColor: 'rgb(255, 99, 132)',
-        //             data: [19016, 19736, 19125, 19397, 19530, 19334],
-        //         }]
-        //     };
-        
-        //     const config = {
-        //         type: 'line',
-        //         data: data,
-        //         options: {}
-        //     };
-        //     const mychart = new Chart(document.querySelector('#chart'),config)
-        // }
+        async getChart(id){
+            const canvasDiv = this.model.firstElementChild
+            if(!canvasDiv){
+                return
+            }
+            canvasDiv.innerHTML = ''
+            const canvas = document.createElement('canvas')
+            canvasDiv.append(canvas)
+            let chartData = ''
+            const labels = []
+            const prize = []
+            await fetch(`https://api.coincap.io/v2/assets/${id}/history?interval=d1`)
+            .then(value=>value.json()
+            .then(value=>chartData = value.data.slice(-10))
+            )
+            chartData.forEach(value=>{
+                prize.push(value.priceUsd)
+                labels.push(value.date.slice(0,10))
+            })
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: "Prize",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: prize,
+                }]
+            };
+            const config = {
+                type: 'line',
+                data: data,
+                option:{}
+            };
+            this.model.style.display = 'flex'
+            document.scroll = 'false'
+            this.animationZoomIn(this.model.firstElementChild, 1)
+            new Chart(canvas,config)
+        },
+        animationZoomIn(element, zoom, x = 0.5){
+            if (x < zoom) {
+                x += 0.10
+                element.style.transform = `scale(${x})`
+                setTimeout(() => {
+                    this.animationZoomIn(element, zoom, x)
+                }, 5);
+            }
+        }
     }
     crypto.getData()
     crypto.addListeners()
-    // crypto.getChart()
 })()
